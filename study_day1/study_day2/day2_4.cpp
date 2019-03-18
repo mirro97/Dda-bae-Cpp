a99e2770071772b18bdebc402407c1139b02fd5e
mirro97 <mirro97@naver.com>
2019-03-18 17:58:50 +0900

    C++ study 2_4 :: local range uploaded

---
+++ study_day1/study_day2/day2_4.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+
+void doSomething(int x) 
+{
+	x = 123;
+	cout << x << endl;
+}
+
+int main() 
+{
+	int x = 0;
+	
+	/*cout << x << " " << &x << endl;
+
+	{//영역을 분리해줌
+		int x = 1;
+		cout << x << " " << &x << endl;
+	}
+
+	{
+		int x = 2;
+	}
+	//int x(0);// x = 0;
+	*/
+
+	cout << x << endl;
+	doSomething(x);
+	cout << x << endl;
+	return 0;
+} 
